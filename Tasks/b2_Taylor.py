7c8b33b5a4e26899830b68131ce76576463d8fd6
PYTHON <7@ya.ru>
2019-12-19 20:53:31 +0300

    added taylor

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,7 @@
 Taylor series
 """
 from typing import Union
+import math
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,6 +12,21 @@ def ex(x: Union[int, float]) -> float:
 	:param x: x value
 	:return: e^x value
 	"""
+
+	if x == 0:
+		return 1
+	else:
+		result_sum = 1
+		n = 1
+		delta = 0.00001
+		while True:
+			result = (x**n) / math.factorial(n)
+			if result < delta:
+				return result_sum
+			else:
+				result_sum += result
+				n += 1
+
 	print(x)
 	return 0
 
@@ -22,5 +38,8 @@ def sinx(x: Union[int, float]) -> float:
 	:param x: x value
 	:return: sin(x) value
 	"""
+
+	
 	print(x)
 	return 0
+
