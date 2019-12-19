0067f04ec023ced3d1bcb7da5fd31c75a81b12db
PYTHON <7@ya.ru>
2019-12-19 18:42:21 +0300

    added linear search

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -2,8 +2,8 @@
 This module implements some functions based on linear search algo
 """
 from typing import Sequence
-
-
+import numpy as np
+import random
 def min_search(arr: Sequence) -> int:
 	"""
 	Function that find minimal element in array
@@ -13,3 +13,21 @@ def min_search(arr: Sequence) -> int:
 	"""
 	print(arr)
 	return -1
+
+def find(find_elem, array):
+
+	index = None
+	for i, elem in enumerate(array):
+		if elem == find_elem:
+			index = i
+			return index
+	return index
+
+
+
+if __name__ == "__main__":
+	r = 1000
+	array = np.random.randint(0, r, r)
+	find_elem = np.random.choice(array)
+	print(find_elem)
+	print(find(find_elem, array))
