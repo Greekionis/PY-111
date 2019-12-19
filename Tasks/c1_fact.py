3a967fe6a813117a3ac74a917324e509b768692e
PYTHON <7@ya.ru>
2019-12-19 21:20:24 +0300

    added factorial

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -4,6 +4,14 @@ def factorial_recursive(n: int) -> int:
 	:param n: int > 0
 	:return: factorial of n
 	"""
+	if n < 0:
+		raise ValueError
+	elif n == 1:
+		return 1
+	else:
+		return factorial_recursive(n - 1) * n
+
+
 	print(n)
 	return 0
 
@@ -15,5 +23,19 @@ def factorial_iterative(n: int) -> int:
 	:param n: int > 0
 	:return: factorial of n
 	"""
+
+	if n < 0:
+		raise ValueError
+	else:
+		if n == 0:
+			return 1
+		else:
+			p = 1
+			for i in range(1, n+1):
+				p *= i
+			return p
+
 	print(n)
-	return 0
+	#return 0
+
+print(factorial_iterative(5))
